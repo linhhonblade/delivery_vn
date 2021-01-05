c94f0186c0ac62182b9fbdaf67f27c602f0b5c02
linhhonblade <meomaimaimeo@gmail.com>
2021-01-05 18:08:56 +0700

    first config for ahamove shipping

---
+++ models/res_config_settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    module_delivery_ahamove = fields.Boolean(string='Integrate Odoo with Ahamove Shipping')
