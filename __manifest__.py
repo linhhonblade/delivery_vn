d6f24f58ccc6f53c4ff577c547ec481ec8dfaf5b
linhhonblade <meomaimaimeo@gmail.com>
2021-01-05 18:08:56 +0700

    first config for ahamove shipping

--- __manifest__.py
+++ __manifest__.py
@@ -3,33 +3,35 @@
     'name': "delivery_vn",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Vietnam Delivery Methods""",
 
     'description': """
-        Long description of module's purpose
+        A base set-up for Vietname delivery methods
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "mailovemisa",
+    'website': "https://github.com/linhhonblade",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Operations/Inventory/Delivery',
+    'version': '13.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['stock'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/res_config_settings_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': False,
 }
