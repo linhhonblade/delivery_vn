27b033d8e4223c537cdcfd2a7a04b64b45872ef2
linhhonblade <meomaimaimeo@gmail.com>
2021-03-15 17:26:57 +0700

    add config for ghn module, disable get available services function to make ahamove compatible again. Fix later

--- models/delivery_carrier.py
+++ models/delivery_carrier.py
@@ -10,12 +10,12 @@ class DeliveryCarrier(models.Model):
     @api.model
     def available_carriers(self, partner):
         available_carriers = super(DeliveryCarrier, self).available_carriers(partner)
-        if hasattr(self, 'ahamove_get_avail_services'):
-            filtered = available_carriers.filtered(lambda r: r.delivery_type != 'ahamove')
-            ahamove_avail_services_list = getattr(self, 'ahamove_get_avail_services')(partner)
-            avail_ahamove_carriers = self.env['delivery.carrier'].search([('service_type.code',
-                                                                           'in',
-                                                                           ahamove_avail_services_list)])
-            available_carriers = filtered | avail_ahamove_carriers
+        # if hasattr(self, 'ahamove_get_avail_services'):
+        #     filtered = available_carriers.filtered(lambda r: r.delivery_type != 'ahamove')
+        #     ahamove_avail_services_list = getattr(self, 'ahamove_get_avail_services')(partner)
+        #     avail_ahamove_carriers = self.env['delivery.carrier'].search([('service_type.code',
+        #                                                                    'in',
+        #                                                                    ahamove_avail_services_list)])
+        #     available_carriers = filtered | avail_ahamove_carriers
         return available_carriers
 
--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -6,3 +6,4 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     module_delivery_ahamove = fields.Boolean(string='Integrate Odoo with Ahamove Shipping')
+    module_delivery_ghn = fields.Boolean(string='Integrate Odoo with GHN Shipping')
